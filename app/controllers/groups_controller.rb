8c7ffa949da203b289f9f84e68d57599b02aa606
Toshio Kaneda <toshiokaneda@ToshionoMacBook-puro.local>
2019-10-16 00:58:20 +0900

    次foreign_keyの設定を質問する

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,25 @@ class GroupsController < ApplicationController
   def index
   end
 
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = User.new(group_params)
+    if @group.save
+    flash[:notice] = '無事登録できました'
+    redirect_to root_path
+    else
+    render :new
+    end
+
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] } )
+  end
+
 end
