b97635adfe958527879b33821cb24d2bbd228a4a
Toshio Kaneda <toshiokaneda@ToshionoMacBook-puro.local>
2019-11-21 18:58:28 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :application, 'chat-space2'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:TOSHIOKANEDA/chat-space2.git'
 
-
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
